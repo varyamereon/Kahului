faf6317ba0aa33ee292fee94faa08b08dbdc7bea
Will Beal <willbeal@outlook.com>
2022-10-25 16:57:35 +0200

    Added Geo

---
+++ Geo/Helpers/Helpers.cs
@@ -0,0 +1,8 @@
+﻿namespace Kahului.Geo.Helpers;
+
+internal static class Helpers
+{
+    internal static double Deg2Rad(double deg) => (deg * Math.PI / 180.0);
+
+    internal static double Rad2Deg(double rad) => (rad / Math.PI * 180.0);
+}
